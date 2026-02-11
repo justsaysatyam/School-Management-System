77700af3af0367f9acb9850f9e9fb945ecf33c4d
Satyam <justsaysatyam@gmail.com>
2026-02-11 15:30:06 +0530

    error solve admin

--- school_management/settings.py
+++ school_management/settings.py
@@ -59,13 +59,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'school_management.wsgi.application'
 
+import dj_database_url
+
 # Database
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
