1d2ab92d2a22f69545405234560640362f2ee38d
Satyam <justsaysatyam@gmail.com>
2026-02-11 13:34:02 +0530

    code update

--- school_management/settings.py
+++ school_management/settings.py
@@ -85,7 +85,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Media files (Uploads)
 MEDIA_URL = '/media/'
